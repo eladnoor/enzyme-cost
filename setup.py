d3609ac218e60f77e1a9ef67547cc4c6198cade1
Elad Noor <elad.noor@gmail.com>
2015-05-01 16:26:53 +0200

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+
+setup(
+    name = 'EnzymeCost',
+    version = '0.1.0',
+    author = 'Elad Noor',
+    author_email='noor@imsb.biol.ethz.ch',
+    description = 'A collection of tools that perform Enzyme Cost Minimization',
+    license = 'MIT',
+    packages=['ecm'],
+    url='https://github.com/eladnoor/enzyme-cost',
+    install_requires=['scipy>=0.14.0',
+                      'numpy>=1.6.2',
+                      'pulp>=1.5.9'],
+    data_files=[('ecm_ecoli_aerobic', ['data/ecm_ecoli_aerobic.tsv']),
+               ],
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+    
+        # Indicate who your project is intended for
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Chemistry',
+    
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+    
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2.7',
+    ],
+)
+
